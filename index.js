7046b15515a29c6b0e77212d9fcde4ebf11bc733
Hamsa Omar <hamsaomar@infra04-wg012.eduroam.wireless.umn.edu>
2022-11-10 11:12:18 -0600

    removed cors

--- index.js
+++ index.js
@@ -9,19 +9,30 @@ app.use(
   express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 1000000 })
 );
 
+// const allowlist = ["https://freewsj.com"];
+// const corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (allowlist.indexOf(req.header("Origin")) !== -1) {
+//     corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false }; // disable CORS for this request
+//   }
+//   callback(null, corsOptions); // callback expects two parameters: error and options
+// };
+
+// var corsOptions = {
+//   origin: false,
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+
+// app.use(cors(corsOptions));
+
 const prisma = require("./prisma.js");
 
 prisma
   .$connect()
   .then(() => console.log("Prisma Connection established"))
   .catch((err) => console.log(err));
-
-const corsOptions = {
-  origin: ["http://10.131.0.220:3000"],
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
 
 const indexRouter = require("./routes/index.js");
 
